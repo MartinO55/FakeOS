6d10833e47ffcf2f94bdd67498f4f8eee62d6fba
Martin Olson <martinguyolson@gmail.com>
2023-01-05 17:17:22 +1100

    added some javascript. at least it isn't throwing errors, although it isnt doing its job

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
 //addEventListener("dblclick")
+//let opened = null;
+
+// let target = document.getElementById(".textfileModal");
+
+// let process =document.addEventListener("click", openWindow);
+
+// function openWindow() {
+//   target.style.backgroundColor = "red";
+//   console.log(target);
+// }
+
+document.querySelectorAll(".openable").forEach((element) => {
+  element.addEventListener("dblclick", (event) => {
+    //opened = event.target;
+    let targetToChange = document.getElementById("textfileModal");
+    console.log(targetToChange);
+    targetToChange.classList.remove("hidden");
+    targetToChange.classList.add("display");
+    //event.target.classList.remove("hidden");
+    //event.target.classList.add("displayable");
+  });
+});
+
+document.querySelectorAll(".fileBar__close").forEach((element) => {
+  element.addEventListener("click", (event) => {
+    event.target.classList.add("hidden");
+    event.target.classList.remove("display");
+  });
+});
+
+//so this should toggle the target to visible on click (should be dblclick)
+// document.addEventListener("click", (event) => {
+//   if (opened) {
+//     opened.classList.add(".displayable");
+//     opened.classList.remove(".hidden");
+//   }
+// });
